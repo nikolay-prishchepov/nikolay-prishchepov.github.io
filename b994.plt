e49a1d3a033090208b12183726e3f5bfd63b336d
Nikolay Prishchepov <nprishchepov@gmail.com>
2017-01-22 21:01:33 +0300

    first

--- b994.plt
+++ b994.plt
@@ -8,15 +8,11 @@ set timefmt "\"%m/%d/%y %H:%M:%S\""
 set grid
 set xlabel "Day"
 set ylabel "Humidity"
-set y2label "Temperature"
 set format x "%d"
-set xtics 345600
+set xtics 86400
 set ytics 100
 set yrange [0:900]
-set y2tics 6
-set y2range [0:54]
 set style fill transparent solid 0.7 noborder
 set term png truecolor enhanced size 1024,300
-set output "/home/pi/www/cactus_1.png"
-plot "/home/pi/www/cactus_1.csv" u 1:2       lc rgb '#0062ff' w boxes  title 'Humidity ' , \
-     "/home/pi/www/cactus_1.csv" u 1:($3)    smooth bezier lc rgb '#ff0000' lw 2 w line  axis x1y2 title 'Temperature'
+set output "/home/pi/cactus/b994.png"
+plot "/home/pi/cactus/b994.csv" u 1:2       lc rgb '#0062ff' w boxes  title 'Humidity ' 
--- b994.py
+++ b994.py
@@ -5,13 +5,13 @@ import time
 
 gatt=pexpect.spawn('gatttool -I')
 gatt.logfile = open("pylog.txt", "w")
-gatt.sendline('connect 7C:EC:79:50:CE:54')
+gatt.sendline('connect 54:4A:16:2F:B9:94')
 gatt.expect('Connection successful')
 gatt.expect('Notification handle.*')
 bad_string = gatt.after
 gatt.sendline('disconnect')
 
-bad_string = bad_string.split()[5:14]
+bad_string = bad_string.split()[5:11]
 
 good_string = time.strftime('\"%D %T\"') + " "
 
@@ -19,12 +19,12 @@ for d in bad_string:
   good_string += chr(int(d,16))
 
 
-good_string += '\n'
+#good_string += '\n'
 
-print good_string
+#print good_string
 
-#f = open('/home/pi/www/cactus_1.csv','a')
-#f.write(good_string)
-#f.close()
+f = open('/home/pi/cactus/b994.csv','a')
+f.write(good_string)
+f.close()
 
 sys.exit(0)
