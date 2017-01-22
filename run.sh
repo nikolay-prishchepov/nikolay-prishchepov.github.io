bfa8343d97e2920a16201d3ce5b0035a92f6fef7
Nikolay Prishchepov <nprishchepov@gmail.com>
2017-01-23 00:00:11 +0300

    cron

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
 cd /home/pi/cactus
+
 ./ce54.py
 ./ce54.plt
 ./b994.py
 ./b994.plt
 
-eval `ssh-agent -s` 
+eval `ssh-agent -s`
 ssh-add ~/.ssh/*_rsa
 
 /usr/bin/git commit -a -m "cron"
